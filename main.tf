0189cff007c8161cb15f4ffd3a39a826b7609d66
thiernomadjou <thiernomadjou@gmail.com>
2024-04-03 11:39:34 +0200

    "

--- main.tf
+++ main.tf
@@ -17,3 +17,30 @@ resource "aws_s3_bucket" "sample" {
     public_bucket = false
   }
 }
+
+
+resource "aws_s3_bucket_ownership_controls" "example" {
+  bucket = aws_s3_bucket.sample.id
+  rule {
+    object_ownership = "BucketOwnerPreferred"
+  }
+}
+
+resource "aws_s3_bucket_public_access_block" "example" {
+  bucket = aws_s3_bucket.sample.id
+
+  block_public_acls       = false
+  block_public_policy     = false
+  ignore_public_acls      = false
+  restrict_public_buckets = false
+}
+
+resource "aws_s3_bucket_acl" "example" {
+  depends_on = [
+    aws_s3_bucket_ownership_controls.example,
+    aws_s3_bucket_public_access_block.example,
+  ]
+
+  bucket = aws_s3_bucket.sample.id
+  acl    = "public-read"
+}
